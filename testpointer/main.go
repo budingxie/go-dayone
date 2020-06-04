d6a2188b1d17357523be4c8a89e804008d1d27b6
pengyou <pengyou@xiaomi.com>
2020-06-04 09:42:08 +0800

    pointer

---
+++ testpointer/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func testPointer1() {
+	num := 5
+	unmPointer := &num
+	flNum := (*float32)(unsafe.Pointer(unmPointer))
+	fmt.Println(flNum)
+}
+
+func testPointer2() {
+	type Num struct {
+		i string
+		j int64
+	}
+	n := Num{i: "SSADREEEEA", j: 1}
+
+	nPointer := unsafe.Pointer(&n)
+	//niPointer := (*string)(unsafe.Pointer(nPointer))
+	niPointer := (*string)(nPointer)
+	*niPointer = "煎鱼"
+
+	fmt.Println(unsafe.Offsetof(n.j))
+	njPointer := (*int64)(unsafe.Pointer(uintptr(nPointer) + unsafe.Offsetof(n.j)))
+	*njPointer = 2
+	fmt.Printf("n.i: %s, n.j: %d\n", n.i, n.j)
+
+	fmt.Println(unsafe.Offsetof(n.i))
+	ni2Pointer := (*string)(unsafe.Pointer(uintptr(nPointer) + unsafe.Offsetof(n.i)))
+	*ni2Pointer = "水煮"
+	fmt.Printf("n.i: %s, n.j: %d\n", n.i, n.j)
+
+}
+
+func main() {
+	//testPointer1()
+	testPointer2()
+}
